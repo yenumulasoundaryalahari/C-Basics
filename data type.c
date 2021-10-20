9f5532b8a412be90d537e6f633eec87d8ec08501
yenumulasoundaryalahari <92291195+yenumulasoundaryalahari@users.noreply.github.com>
2021-10-20 12:34:51 +0530

    Add files via upload

---
+++ data type.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{ 
+	//variable declaration
+	int age;
+	float cgpa;
+	char initial;
+	//initialization
+	age = 21;
+	cgpa = 8.4;
+	initial = 'R';
+	//output
+	printf("%d\n",age);
+	printf("%f\n",cgpa);
+	printf("%c\n",initial);
+
+}
+
