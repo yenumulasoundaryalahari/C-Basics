abc36227778cef9cc34c90fcd330d9a1ff10b3fb
yenumulasoundaryalahari <92291195+yenumulasoundaryalahari@users.noreply.github.com>
2021-10-26 08:35:55 -0700

    Add files via upload

---
+++ phy.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+	int phy,che,mat,eng,tot;
+	phy=50;
+	che=60;
+	mat=70;
+	eng=40;
+	tot=phy+che+mat+eng;
+	printf("marks in physics :%d\n",phy);
+	printf("marks in chemistry :%d\n",che);
+	printf("marks in maths :%d\n",mat);
+	printf("marks in english :%d\n",eng);
+	printf("total marks :%d",tot);
+}
