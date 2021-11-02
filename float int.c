baad307436d2500f4a127c07a2bbcc51c991c076
yenumulasoundaryalahari <92291195+yenumulasoundaryalahari@users.noreply.github.com>
2021-11-02 12:36:14 +0530

    Add files via upload

---
+++ float int.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+	int a,b;
+	float c;
+	
+	a=11;
+	b=2;
+	
+	c=a*1.0/b;
+	
+	printf("quotient of %d and %d is %f:",a,b,c);
+}
---
+++ remainder.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+	int a,b,c;
+	
+	a=20;
+	b=30;
+	
+	c=a%b;
+	
+	printf("remainder of %d and %d is %d:",a,b,c);
+}
