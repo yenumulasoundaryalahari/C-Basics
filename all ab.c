5c0bc5f50a6a5dcef1d6b2fa5b6284fce7e00347
yenumulasoundaryalahari <92291195+yenumulasoundaryalahari@users.noreply.github.com>
2021-11-08 12:35:19 +0530

    Add files via upload

---
+++ all ab.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+	int a,b;
+	a=20;
+	b=10;
+	printf("%d > %d --> %d\n",a,b,a>b);
+	printf("%d < %d --> %d\n",a,b,a<b);
+	printf("%d == %d --> %d\n",a,b,a==b);
+	printf("%d != %d --> %d\n",a,b,a!=b);
+	printf("%d >= %d --> %d\n",a,b,a>=b);
+	printf("%d <= %d --> %d\n",a,b,a<=b);
+	
+}
---
+++ and.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+	int a,b,c;
+	a=10;
+	b=30;
+	c=20;
+	printf("%d",(10<30) && (20<30));
+
+}
